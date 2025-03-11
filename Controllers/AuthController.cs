8e3c91443e72553b893c129966b61ab3eb3d5528
kirill <Defoult@gmail.com>
2025-03-12 02:56:51 +0300

    Создал контороллер для авторизации, а также view у нему

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        public IActionResult register()
+        {
+            return View();
+        }
+    }
+}
